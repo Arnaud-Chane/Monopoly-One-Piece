7101bea23e9997fbd3e4a3a97ff8cd0aaf37045a
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-07-21 18:44:20 +0200

    testturn

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
  var beginningSpeech = "Hello ! Let's get started !";
- console.log = beginningSpeech;
+ console.log(beginningSpeech);
+ 
+ var j1Turn;
+ var j2Turn;
+ var j3Turn;
+ 
+ var inGamePlayer = [
+ 	j1Turn = true,
+ 	j2Turn = false,
+ 	j3Turn = false
+ ];
+ 
+ console.log(inGamePlayer);
+ 
