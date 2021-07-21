60df7369e101c72da0c1a2c5ad92f8e572e0c09f
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-07-21 23:04:46 +0200

    test jturn

--- test.js
+++ test.js
@@ -1,15 +1,24 @@
- var beginningSpeech = "Hello ! Let's get started !";
- console.log(beginningSpeech);
+var beginningSpeech = "Hello ! Let's get started !";
+console.log(beginningSpeech);
  
- var j1Turn;
- var j2Turn;
- var j3Turn;
+var j1Turn;
+var j2Turn;
+var j3Turn;
  
- var inGamePlayer = [
+var playerInGame = [
  	j1Turn = true,
  	j2Turn = false,
  	j3Turn = false
- ];
+];
  
- console.log(inGamePlayer);
+console.log(playerInGame);
+ 
+var j1 = [
+ 	j1Turn = false
+];
+ 
+ 
+console.log(playerInGame);
+ 
+var myMove = prompt("What's your next move ?");
  
