fcd3773ac71a3fe6fd2ba31229df7b0f3cd23374
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-07-22 08:43:00 +0200

    cleanTest

--- playerTurn.js
+++ playerTurn.js
@@ -10,4 +10,28 @@ function check1Turn () {
 
 
 	
-	
+	var beginningSpeech = "Hello ! Let's get started !";
+console.log(beginningSpeech);
+ 
+var j1Turn;
+var j2Turn;
+var j3Turn;
+ 
+var playerInGame = {
+ 	j1Turn : true,
+ 	j2Turn : false,
+ 	j3Turn : false
+};
+ 
+console.log(playerInGame);
+
+var j1 = {
+ 	j1Turn : false
+};
+ 
+ 
+console.log(playerInGame);
+ 
+var myMove = prompt("What's your next move ?");
+ 
+// not good write with [] instead of {}=
