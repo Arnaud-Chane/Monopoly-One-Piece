7687dcdd1120194e757b3cb9a44889ef9cd7f273
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-07-26 22:41:31 +0200

    turnTest

--- playerTurn.js
+++ playerTurn.js
@@ -96,5 +96,44 @@ ooor :
 
 
 
+function premierFiltre(joueur) {
+ 
+};
+
+function grosFiltre(){
+ return 
+}
+
+
+var allPlayer = [
+	j1 = [
+		true,
+		avatar = "Chien",
+		money = 50
+	],
+	
+	j2 = [
+		false,
+		avatar = "Chat",
+		money = 10
+	],
+	
+	j3 = [
+	 false,
+	 avatar = "Requin",
+	 money = 100
+	]
+];
+
+
+
+var resultat = allPlayer.forEach(petitFiltre);
+console.log(resultat);
+
+
+
+
+
+
 
 
