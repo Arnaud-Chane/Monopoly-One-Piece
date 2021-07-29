f3ed9d5407600713373705a265d89f4047355fc2
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-07-29 13:53:39 +0200

    whipe

--- test.js
+++ test.js
@@ -2,26 +2,31 @@ var actualTurn = 0;
 var i = 0;
 i = prompt("How many player ?");
 
+var action;
+
 /*
 ("So there are " + i + " players");
 mettre if player + (i+1);
 
 
 */
+function actionTurn() {
+	alert("It's your turn Player" + actualTurn + ".");
+ 	action = prompt("What are you gonna do ?");
+};
+
+
 
 var namePlayerTurn;
 
-if (actualTurn => 0) {
+if (actualTurn => 1) {
+	actionTurn();
 	actualTurn += 1;
-} if (actualTurn > i) {
+} if (actualTurn > i+1) {
+	actionTurn();
 	actualTurn = 0;
 };
 
-function actionTurn() {
-	alert("It's your turn Player" + actualTurn + ".");
-	alert("What are you gonna do ?");
-};
-
-
+// mettre while ppur que les fonctions tournent en boucle
 
 
