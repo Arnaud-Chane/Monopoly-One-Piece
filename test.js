75c32c4d5ee0457503c9253d2b452c913ad3d91f
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-08-10 18:47:03 +0200

    turret

--- test.js
+++ test.js
@@ -1,13 +1,19 @@
-var answer1 = [10, 20];
-var answer2 = "Bonjour";
-var answer3 = [10, 20];
+// Turret property
+var turretNumber = [];
+var turretPosX = [];
+var turretPosY = [];
 
-var answer4 = answer1 === answer2;
-alert(answer4);
+// all variable to add turrets
+var answerPlayer;
 
-answer1.join('');
-answer3.join('');
-var answer5 = answer1 === answer3;
-alert(answer1);
-alert(answer3);
-alert(answer5);
+
+// function to add a turret and give its position
+function addTurret () {
+	if (answerPlayer.includes("create" && "turret")) {
+		// array.length > lastArrayLength
+	}
+};
+
+function playerAction () {
+	answerPlayer = prompt("What's next ?");
+};
