b7f6c68203d1b19b267de405c0bf3a984f38d3fd
SirArnaUD <arnaud.chane.dev@gmail.com>
2021-08-11 08:34:26 +0200

    addturret

--- test.js
+++ test.js
@@ -5,15 +5,26 @@ var turretPosY = [];
 
 // all variable to add turrets
 var answerPlayer;
-
+var actualLengthArray;
+var lastLengthArray;
 
 // function to add a turret and give its position
 function addTurret () {
 	if (answerPlayer.includes("create" && "turret")) {
 		// array.length > lastArrayLength
-	}
+		turretNumber.push(prompt("Turret Name ?"));
+		// on pourra mettre ensuite :
+		// how many turret ? et on pourra rajouter directement le nombre sans se soucier des noms
+	};
 };
 
 function playerAction () {
 	answerPlayer = prompt("What's next ?");
 };
+
+
+// Beginning of the function
+playerAction ();
+addTurret ();
+alert (turretNumber);
+
